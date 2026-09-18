3b99027b209ba43aa013804ea42ce7a0e994dd5b
EricMacarroCanton <ericmacarro6@gmail.com>
2026-09-18 19:32:52 +0200

    main

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -5,10 +5,26 @@ public class Rot13 {
     public char[] majuscules = lletres.toCharArray();
 
     public static String xifraRot13(String cadena) {
-    
-return "hola";
+    if (cadena == null) return null;
+          
 }
-    public static void main(String[] args) {
+public static String desxifraRot13(String cadena) {
+    if (cadena == null) return null;
+}
 
-}
+public static void main(String[] args) {
+        String original1[] = {"ABC" , "XYZ", "Hola, Mr. calçot", "Perdó, per tu què és?",};
+        String msgsxifrats[] = new String[original1.length];
+
+        System.out.println("\nXifrat---");
+        for (int i = 0; i < original1.length; i++) {
+            msgsxifrats[i] = xifraRot13(original1[i]);
+            System.out.printf("%-23sc=> %s%n" , original1[i], msgsxifrats[i]);
+        }
+        System.out.println("\nDesxifrat---");
+
+            for(String msg : msgsxifrats) {
+                System.out.printf("%-23sc=> %s%n" , msg, desxifraRot13(msg));
+            }
+   }
 }
