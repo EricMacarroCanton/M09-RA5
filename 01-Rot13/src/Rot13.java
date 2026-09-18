38df26ae5bc07209ea0262e5240066ba56ff1cc9
EricMacarroCanton <ericmacarro6@gmail.com>
2026-09-18 19:12:01 +0200

    1er commit

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -1,18 +1,14 @@
 public class Rot13 {
-    public static void main(String[] args) {
     
-    
-char[] majuscules = {
-    'A', 'À', 'Á', 'B', 'C', 'Ç', 'D', 'E', 'É', 'È', 'F', 'G', 'H',
-    'I', 'Í', 'Ì', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'Ó', 'Ò', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ù', 'V', 'W', 'X', 'Y', 'Z'
-};
+    private static final String lletres = "AÀÁBCÇDEÉÈFGHIÍÌJKLMNÑOÓÒPQRSTUÚÜÙVWXYZ";
+    public char[] minuscules = lletres.toLowerCase().toCharArray();
+    public char[] majuscules = lletres.toCharArray();
 
-char[] minuscules = {
-    'a', 'à', 'á', 'b', 'c', 'ç', 'd', 'e', 'é', 'è', 'f', 'g', 'h',
-    'i', 'í', 'ì', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'ó', 'ò', 'p',
-    'q', 'r', 's', 't', 'u', 'ú', 'ü', 'ù', 'v', 'w', 'x', 'y', 'z'
-};
+    public static String xifraRot13(String cadena) {
+    
+return "hola";
+}
+    public static void main(String[] args) {
 
 }
 }
