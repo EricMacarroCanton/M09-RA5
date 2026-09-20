7bcaf5a6750e899431d5cdf8747b4dd0c4053129
EricMacarroCanton <ericmacarro6@gmail.com>
2026-09-20 16:58:45 +0200

    DESXIFRA  METODE

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -41,7 +41,41 @@ public class Rot13 {
         
 public static String desxifraRot13(String cadena) {
     if (cadena == null) return null;
-}
+  String resultat = "";
+
+        for (int i = 0; i < cadena.length(); i++) {
+            char lletra = cadena.charAt(i);
+            boolean trobat = false;
+
+            for (int j = 0; j < minuscules.length; j++) {
+                if (minuscules[j] == lletra) {
+                    int novaPos = (j - 13) % minuscules.length;
+                    if (novaPos < 0) novaPos += minuscules.length;
+                    resultat += minuscules[novaPos];
+                    trobat = true;
+                    break;
+                }
+            }
+
+            if (!trobat) {
+                for (int j = 0; j < majuscules.length; j++) {
+                    if (majuscules[j] == lletra) {
+                        int novaPos = (j - 13) % majuscules.length;
+                        if (novaPos < 0) novaPos += majuscules.length;
+                        resultat += majuscules[novaPos];
+                        trobat = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!trobat) {
+                resultat += lletra;
+            }
+        }
+
+        return resultat;
+    }
 
 public static void main(String[] args) {
         String original1[] = {"ABC" , "XYZ", "Hola, Mr. calçot", "Perdó, per tu què és?",};
