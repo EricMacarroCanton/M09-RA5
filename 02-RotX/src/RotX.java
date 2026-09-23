2ebaffb8d1f88d3203e552c0289580b67c7ab924
EricMacarroCanton <ericmacarro6@gmail.com>
2026-09-23 18:16:44 +0200

    Creació de RotX

---
+++ 02-RotX/src/RotX.java
@@ -0,0 +1,21 @@
+public class RotX {
+    
+private static final String lletres = "AÁÀBCÇDEÉÈFGHIÍÌÏJKLMNÑOÓÒPQRSTUÚÙÜVWXYZ";
+    public static char[] minuscules = lletres.toLowerCase().toCharArray();
+    public static char[] majuscules = lletres.toCharArray();
+public static String xifraRotX (String Cadena, int desplaçament) {
+
+    return "s";
+}
+
+public static String desxifraRotX (String Cadena, int desplaçament) {
+
+    return "s";
+}
+
+public static String forcaBrutaRotX (cadenaXifrada) {
+
+    return "s";
+}
+
+}
